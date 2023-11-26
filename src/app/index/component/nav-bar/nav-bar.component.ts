f69bd43f77b9d1c530578f7e282221068493627d
sirachuch <sirachuch.kotuta@gmail.com>
2023-11-26 22:45:38 +0700

    demo collapse

--- src/app/index/component/nav-bar/nav-bar.component.ts
+++ src/app/index/component/nav-bar/nav-bar.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class NavBarComponent {
 
+
+  public routeLogin(){
+    window.location.href ='auth/login'
+  }
 }
--- src/app/index/pages/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/index/pages/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from 'src/app/index/services/local-storage.service';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.scss']
 })
 export class AdminDashboardComponent {
-
+  first_name: any
+  lastname: any
+  collapse_basic_menu: boolean = false
+  collapse_payment_menu: boolean = false
+  collapse_report_menu: boolean = false
+  constructor(private localStorageService: LocalStorageService) {
+    this.first_name = this.localStorageService.getFirstname()
+    this.lastname = this.localStorageService.getLastname()
+  }
+  setCollapsebasic() {
+    // const collapse_transition = document.getElementsByClassName('wrapper-collapse-transition')
+    if (this.collapse_basic_menu == true) {
+      return this.collapse_basic_menu = false
+    }
+    else {
+      return this.collapse_basic_menu = true
+    }
+  }
+  setCollapsepayment() {
+    if (this.collapse_payment_menu == true) {
+      return this.collapse_payment_menu = false
+    }
+    else {
+      return this.collapse_payment_menu = true
+    }
+  }
+  setCollapsereport() {
+    if (this.collapse_report_menu == true) {
+      return this.collapse_report_menu = false
+    }
+    else {
+      return this.collapse_report_menu = true
+    }
+  }
 }
