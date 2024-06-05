95867c1dd7dca4d524ff22cb7a9a2d8e888e5336
Sirachuch-dev <sirachuch.kotuta@gmail.com>
2024-06-05 23:28:20 +0700

    custom remove icon

--- src/app/index/component/remove-icon/remove-icon.component.ts
+++ src/app/index/component/remove-icon/remove-icon.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, Renderer2, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-remove-icon',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./remove-icon.component.scss']
 })
 export class RemoveIconComponent {
+  @Input() customStyle = {
+    top: '',
+    right: ''
+  }
+  @ViewChild('icon') customIcon!: ElementRef;
+  constructor(private renderer: Renderer2) { }
+  ngOnInit(): void {
 
+  }
+
+  ngAfterViewInit(): void {
+    if(this.customStyle){
+      this.renderer.setStyle(this.customIcon?.nativeElement, 'top', this.customStyle.top)
+      this.renderer.setStyle(this.customIcon?.nativeElement, 'right', this.customStyle.right)
+    }
+  }
 }
