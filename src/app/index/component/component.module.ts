b9119e1dd5764c221c7bb8bd429faeeb83cd4e96
Sirachuch-dev <sirachuch.kotuta@gmail.com>
2024-06-05 23:28:39 +0700

    date picker range return date success

--- src/app/index/component/component.module.ts
+++ src/app/index/component/component.module.ts
@@ -14,6 +14,7 @@ import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { MatInputModule } from '@angular/material/input';
     MatMomentDateModule,
     MatNativeDateModule,
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    ReactiveFormsModule
   ],
   exports: [
     NavBarComponent,
--- src/app/index/component/date-picker-range/date-picker-range.component.ts
+++ src/app/index/component/date-picker-range/date-picker-range.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-date-picker-range',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./date-picker-range.component.scss'],
 })
 export class DatePickerRangeComponent {
+  InitForm: FormGroup
 
+  @Output() currentPage = new EventEmitter<{ startDate: any, endDate: any }>();
+  constructor(private _fb: FormBuilder) {
+    this.InitForm = this._fb.group({
+      start: '',
+      end: ''
+    })
+  }
+
+  cleanGMT(dateGMT: any) {
+    const date = moment(dateGMT).locale("th").format('DD-MM-YYYY');
+    return date
+  }
+
+  setDate() {
+    let start = this.cleanGMT(this.InitForm.get('start')?.value)
+    let end = this.cleanGMT(this.InitForm.get('end')?.value,)
+    this.currentPage.emit(
+      {
+        startDate: start,
+        endDate: end,
+      }
+    )
+  }
+  
+
+  clearDate() {
+    this.InitForm.reset()
+  }
 }
